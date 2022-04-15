e1fc439da259206a944cd3fc5f5863e38d6a62f6
Filip Czaplicki <github@starsep.com>
2022-04-16 00:40:06 +0200

    umWarszawa2GeoJSON

--- umToCSV.py
+++ umToCSV.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-import csv
+import geojson
+from geojson import Feature, Point, FeatureCollection
 from hashlib import md5
 from typing import Tuple, List
 
@@ -19,7 +20,7 @@ DEFAULT_TAG = ("amenity", "restaurant")
 DISTANCE_THRESHOLD = 100.0
 
 
-class UMWarszawaToCSV:
+class UMWarszawaToGeoJSON:
     def __init__(self, cacheEnabled=True):
         self.wgs84Geod = Geod(ellps="WGS84")
         self.transformer = Transformer.from_crs(CRS("epsg:2178"), "wgs84")
@@ -29,23 +30,26 @@ class UMWarszawaToCSV:
 
     @staticmethod
     def addQuotesToJSONKeys(data: str) -> str:
-        r = re.compile(r'(?P<separator>[{,])(?P<key>[a-zA-Z]+):')
+        r = re.compile(r"(?P<separator>[{,])(?P<key>[a-zA-Z]+):")
         return r.sub(r'\g<separator>"\g<key>":', data)
 
     @staticmethod
     def downloadData(theme: str) -> str:
-        return requests.post("https://mapa.um.warszawa.pl/mapviewer/foi", dict(
-            request="getfoi",
-            version="1.0",
-            bbox="0:1787369:9500502:9791137",
-            width=1,
-            height=1,
-            theme=theme,
-            dstsrid=2178,
-            cachefoi="yes",
-            tid="2_7791522485845669270",
-            aw="no",
-        )).text
+        return requests.post(
+            "https://mapa.um.warszawa.pl/mapviewer/foi",
+            dict(
+                request="getfoi",
+                version="1.0",
+                bbox="0:1787369:9500502:9791137",
+                width=1,
+                height=1,
+                theme=theme,
+                dstsrid=2178,
+                cachefoi="yes",
+                tid="2_7791522485845669270",
+                aw="no",
+            ),
+        ).text
 
     def downloadDataWithCache(self, theme: str):
         rawDir = Path("raw")
@@ -57,15 +61,23 @@ class UMWarszawaToCSV:
         if not rawOutputPath.exists() or not self.cacheEnabled:
             rawOutputPath.write_text(self.downloadData(theme))
         if not jsonOutputPath.exists() or not self.cacheEnabled:
-            jsonOutputPath.write_text(self.addQuotesToJSONKeys(rawOutputPath.read_text()))
+            jsonOutputPath.write_text(
+                self.addQuotesToJSONKeys(rawOutputPath.read_text())
+            )
         umData = json.loads(jsonOutputPath.read_text())["foiarray"]
-        return list(map(lambda p: dict(x=p["x"], y=p["y"]), umData))
+        points = []
+        for point in umData:
+            tags = {
+                k: v
+                for k, v in map(lambda x: x.split(": "), point["name"].split("\n"))
+                if v != ""
+            }
+            points.append(dict(x=point["x"], y=point["y"], tags=tags))
+        return points
 
     @staticmethod
     def elementQuery(overpassQuery: List[Tuple[str, str]]):
-        return "".join([
-            f'["{tag}"="{value}"]' for (tag, value) in overpassQuery
-        ])
+        return "".join([f'["{tag}"="{value}"]' for (tag, value) in overpassQuery])
 
     def downloadOverpassData(self, overpassQuery: List[Tuple[str, str]]):
         overpassDir = Path("overpass")
@@ -86,19 +98,21 @@ class UMWarszawaToCSV:
             for node in overpassResult.nodes:
                 result.append(dict(lat=float(node.lat), lon=float(node.lon)))
             for way in overpassResult.ways:
-                result.append(dict(lat=float(way.center_lat), lon=float(way.center_lon.real)))
+                result.append(
+                    dict(lat=float(way.center_lat), lon=float(way.center_lon.real))
+                )
             json.dump(result, overpassOutputPath.open("w"))
         return json.load(overpassOutputPath.open("r"))
 
     @staticmethod
-    def writeCSV(theme: str, outputTag: Tuple[str, str], data):
-        csvDir = Path("output")
-        csvDir.mkdir(exist_ok=True)
-        csvOutputPath = csvDir / (theme + ".csv")
-        csvWriter = csv.writer(csvOutputPath.open("w"), delimiter=";", quotechar='"')
-        csvWriter.writerow(["latitude", "longitude", outputTag[0]])
-        for (lat, lng) in data:
-            csvWriter.writerow([lat, lng, outputTag[1]])
+    def writeOutput(theme: str, data):
+        outputDir = Path("output")
+        outputDir.mkdir(exist_ok=True)
+        outputPath = outputDir / (theme + ".geojson")
+        features = []
+        for (lng, lat, tags) in data:
+            features.append(Feature(geometry=Point((lat, lng)), properties=tags))
+        geojson.dump(FeatureCollection(features), outputPath.open("w"))
 
     def removeLikelyDuplicates(self, overpassData, umData):
         result = []
@@ -106,18 +120,22 @@ class UMWarszawaToCSV:
             lat, lng = self.transformer.transform(point["y"], point["x"])
             minDistance = DISTANCE_THRESHOLD + 1
             for osm in overpassData:
-                minDistance = min(minDistance, self.wgs84Geod.inv(lng, lat, osm["lon"], osm["lat"])[2])
+                minDistance = min(
+                    minDistance, self.wgs84Geod.inv(lng, lat, osm["lon"], osm["lat"])[2]
+                )
                 if minDistance <= DISTANCE_THRESHOLD:
                     break
             if minDistance > DISTANCE_THRESHOLD:
-                result.append((lat, lng))
+                result.append((lat, lng, point["tags"]))
         return result
 
-    def process(self, theme: str, overpassQuery: List[Tuple[str, str]], outputTag: Tuple[str, str] = DEFAULT_TAG):
+    def process(self, theme: str, overpassQuery: List[Tuple[str, str]]):
         umData = self.downloadDataWithCache(theme)
         overpassData = self.downloadOverpassData(overpassQuery=overpassQuery)
-        deduplicatedData = self.removeLikelyDuplicates(overpassData=overpassData, umData=umData)
-        self.writeCSV(theme=theme, data=deduplicatedData, outputTag=outputTag)
+        deduplicatedData = self.removeLikelyDuplicates(
+            overpassData=overpassData, umData=umData
+        )
+        self.writeOutput(theme=theme, data=deduplicatedData)
 
 
 def main():
@@ -125,6 +143,13 @@ def main():
     SPORT_UNKNOWN = ("sport", "fake")
     FITNESS_CENTRE = ("leisure", "fitness_centre")
     dataSets: List[Tuple[str, str]] = [
+        (
+            "dane_wawa.K_ZTM_BILETOMATY_STACJONARNE",
+            [("vending", "public_transport_tickets")],
+        ),
+        ("dane_wawa.ZEZWOLENIA_ALKOHOLOWE_GASTRO", [("amenity", "restaurant")]),
+        # Rowery
+        ("dane_wawa.ROWERY_STOJAKI_ROWEROWE", [("amenity", "bicycle_parking")]),
         # Place zabaw
         ("dane_wawa.I_PLACE_ZABAW_POINT", [("leisure", "playground")]),
         # Sport
@@ -133,7 +158,10 @@ def main():
         ("dane_wawa.S_HALE_SPORTOWE", [("leisure", "sports_hall")]),
         ("dane_wawa.S_INNE", [SPORT_UNKNOWN]),
         ("dane_wawa.S_KORTY_TENISOWE", [("sport", "tennis")]),
-        ("dane_wawa.S_KOSZYKOWKA", [("leisure", "pitch")]), # [("sport", "basketball")]),
+        (
+            "dane_wawa.S_KOSZYKOWKA",
+            [("leisure", "pitch")],
+        ),  # [("sport", "basketball")]),
         ("dane_wawa.S_KREGIELNIE", [("sport", "9pin")]),
         ("dane_wawa.S_LODOWISKA", [("sport", "ice_skating")]),
         ("dane_wawa.S_PCHNIECIE_KULA", [SPORT_UNKNOWN]),
@@ -155,15 +183,11 @@ def main():
         ("dane_wawa.S_STADIONY_LA", [SPORT_ATHLETICS]),
         ("dane_wawa.S_STRZELNICE", [("sport", "shooting")]),
         ("dane_wawa.S_TORY", [SPORT_UNKNOWN]),
-
-        # Rowery
-        ("dane_wawa.ROWERY_STOJAKI_ROWEROWE", [("amenity", "bicycle_parking")]),
-
         # ("", [()]),
         # TODO: ("dane_wawa.I_UJECIA_WOD_PODZIEMNYCH", [()]),
     ]
     for (theme, overpassQuery) in tqdm(dataSets):
-        UMWarszawaToCSV().process(theme=theme, overpassQuery=overpassQuery)
+        UMWarszawaToGeoJSON().process(theme=theme, overpassQuery=overpassQuery)
 
 
 if __name__ == "__main__":
