7b72ef1ac805ae664ac40bb16cc937d4a27c88c5
Filip Czaplicki <github@starsep.com>
2025-07-17 14:25:22 +0200

    nix: uv packages, libs path

--- shell.nix
+++ shell.nix
@@ -1,15 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-  packages = [
-    (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
-      geojson
-      httpx
-      jinja2
-      overpy
-      pyproj
-      pytest
-      requests
-      tqdm
-    ]))
+{
+  pkgs ? import <nixpkgs> { },
+}:
+pkgs.mkShell {
+  packages = with pkgs; [
+    python313
+    uv
+  ];
+  env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+    pkgs.stdenv.cc.cc.lib
+    pkgs.libz
   ];
 }
